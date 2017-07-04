6ef1e00674fefc38e08e5158d848140e7d7b6b43
Hin-Tak Leung <htl10@users.sourceforge.net>
2017-07-04 22:54:06 +0100

    a lot of comments

--- examples/subfonts.py
+++ examples/subfonts.py
@@ -5,9 +5,54 @@
 #  Distributed under the terms of the new BSD license.
 #
 #  Usage:
+#    fontforge -script subfonts.py master-font name-stem SFD-file
+#
+#  Example:
 #    fontforge -script subfonts.py NotoSerifCJKtc-Medium.otf nose Unicode.sfd
 #
-#  Must move /usr/share/fontforge/Adobe-Identity-0.cidmap!
+#  This script requires freetype-py ( https://github.com/rougier/freetype-py ).
+#  On Linux systems, the package may be called "python{,2,3}-freetype" instead.
+#  Also fontforge needs to be built with
+#  "./configure --enable-python-scripting --enable-python-extension" .
+#  This is often the case on Linux, anyway.
+#  The command "echo 'import fontforge' | python" indicates whether
+#  the fontforge python extension is available. This command is silent on
+#  success, but shows "ImportError: No module named fontforge" on failure.
+#
+#  *** Important ***:
+#    To process many Adobe CFF OpenType fonts correctly (e.g. Source/Noto CJK),
+#    the file "Adobe-Identity-0.cidmap" must be moved or renamed to be hidden
+#    from being found by fontforge. On Linux, it is in "/usr/share/fontforge".
+#    See https://github.com/fontforge/fontforge/issues/3084 .
+#    On Ubuntu Linux, this file is in a separate "fontforge-extras" package.
+#    i.e. The Ubuntu "fontforge-extras" package must NOT be installed.
+#
+#  Implementation Notes:
+#    Except for the two blocks of freetype-py dependent code, this is roughly
+#    a line-by-line translation of the "cjk/utils/subfonts/subfonts.pe" script
+#    to fontforge's python API. Notable differences compared to the legacy
+#    scripting interface are:
+#
+#    * Some Python API routines (e.g. "font.generate()" ) takes named
+#      arguments, so there is no need to specify every argument up-to a last
+#      non-default argument.
+#    * The Python routines often take tuples i.e. named lists, for many of
+#      the input arguments which are bit flags.
+#    * The first two arguments to the Python "font.simplify()" is swapped
+#      in position.
+#    * "font.addExtrema()" (without any arguments) seems to have a different
+#      default compared to the legacy interface; not or poorly documented.
+#      See https://github.com/fontforge/fontforge/issues/3105
+#    * Accessing unused encoding slots in Python throws TypeError, instead
+#      of returning False for "font[slot].isWorthOutputting()".
+#      See https://github.com/fontforge/fontforge/issues/3107
+#
+#    * The two blocks of freetype-py code is to work around fontforge's
+#      problem coping with newer CJK fonts having glyphs at multiple encoding
+#      slots.
+#      See https://github.com/fontforge/fontforge/issues/3080 and references
+#      therein.
+#
 # -----------------------------------------------------------------------------
 import fontforge
 from freetype import Face
